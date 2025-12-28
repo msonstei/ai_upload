cdc861c8cf02611cba5bc459a9ef3603c1b3de58
msonstei <mark.sonstein@gmail.com>
2025-12-28 14:03:40 -0900

    added file_name checking

--- docling_load2.py
+++ docling_load2.py
@@ -71,12 +71,12 @@ def pretty_print_POST(req):
         req.body,
     ))
 
-
 def get_embeddings(text_list):
     # e.g., using SentenceTransformers or OpenAI
     return [[0.1] * 768 for _ in text_list] 
 
 def doc_converter(source):
+    # Function to split data into chunks
     converter = DocumentConverter()
     chunker = HierarchicalChunker()
 
@@ -87,7 +87,6 @@ def doc_converter(source):
 
     for i, text in enumerate(texts[:5]):
         print(f"Chunk {i+1}:\n{text}\n{'-'*50}")
-
 
 def upload_to_milvus(file_path, file_name, json_markdown):
     logging.info(f"Milvus uploading {full_path},{file_name}")
@@ -135,6 +134,8 @@ def upload_to_milvus(file_path, file_name, json_markdown):
     #return ml.milvus_loader(OUTPUT_DIR, file_name)
 
 def find_files(directory_path):
+    # Returns the files that will be processed.
+    # Excludes files not listed in  ALLOWED_FILE_TYPES
     for file in glob.glob(os.path.join(directory_path, '**', '*'), recursive=True):
         if os.path.isfile(file) and os.path.splitext(file)[1].lower() in ALLOWED_FILE_TYPES:
             yield file
@@ -180,8 +181,8 @@ def upload_file(token, file_path, api_url, file_name):
     print(f"Uploading {file_path}")
     try:
         with open(file_path, 'rb') as f:
-           files = {'file': f}
-           response = requests.post(api_url, headers=headers, files=files)
+            files = {'file': f}
+            response = requests.post(api_url, headers=headers, files=files)
  
         response.raise_for_status() # Raise an exception for bad status codes
         return response.json()
@@ -191,6 +192,8 @@ def upload_file(token, file_path, api_url, file_name):
         return None
 
 def check_file(file_id,token):
+    # function to check the status of file being processed.
+    # repeated call until it returns 'complete' status
     data = {}
     headers = {
     'Authorization': f'Bearer {token}',
@@ -213,6 +216,28 @@ def check_file(file_id,token):
         logging.DEBUG(f"Open-WebUI Knowledge returned {e}")
         print(f"An error occurred uploading knowledge: {e}")
         return response.raise_for_status()
+
+def check_file_exists(token, api_url, file_name):
+    # Function to check if a file already exists
+    # called before uploading file
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'accept': 'application/json',
+    }
+        
+    params = {
+        'filename': file_name,
+        'content': 'true',
+    }
+    # Get list of uploaded files
+    try:
+        response = requests.post(f'{api_url}/search', headers=headers, params=params)
+        response.raise_for_status() # Raise an exception for bad status codes
+        return response.json()
+    except Exception as e:
+        #logging.DEBUG(f"Open-WebUI returned uploading file")
+        print(f"An error occurred: during upload_file {e}")
+        return None
 
 def get_kname(directory_path):
     # Get the directory name and return it to be used as the knowledge name
@@ -282,8 +307,6 @@ def create_knowledge(kname, token):
         print(f"Error looking up knowledge name: {e}")
         return 1
 
-
-
 def count_files_pathlib(directory_path):
   """Count all files recursively in a directory using pathlib."""
   # Use rglob('*') to find all files and directories recursively
@@ -312,20 +335,23 @@ def upload_files(token, file_path, url, file_name):
 if __name__ == '__main__':
     # Start logging service
     configure_logging(True)
+    # initialize the variables
+    file_id = ''
     response = {'status':'NULL'}
-
     # Create Knowledge Space
     kname = get_kname(directory_path)
     kname = create_knowledge(kname,OPENWEBUI_API_KEY)
     if kname == 1:
-       print("Irrecoverable error in creating knowledge")
-       os._exit
-    print(kname)
+        logging.fatal("Irrecoverable error creating knowledge")
+        print(f"Irrecoverable error in creating knowledge. Check log file {LOG_FILE} for details. Exiting application")
+        os._exit
+    # Assign the returned knowledge id to k_id variable
     k_id = kname['id']
+    logging.info(f"Will add files to knowledge container {k_id}")
     print(f"Will add files to knowledge container {k_id}")
     # Set ne knowledge collection id, use Temp collection if new collection not created
     if k_id:
-       KNOWLEDGE_ENDPOINT = f'http://du-webui/api/v1/knowledge/{k_id}/file/add'
+        KNOWLEDGE_ENDPOINT = f'http://du-webui/api/v1/knowledge/{k_id}/file/add'
 
     # Count the number of files to be processed, and print for user info
     files = find_files(directory_path)
@@ -340,29 +366,37 @@ if __name__ == '__main__':
         new_path = Path(file).parent
         full_path = f"{new_path}{kname['name']}/"
         good_path = f"{new_path}/{file_name}"
-        print(f"Processing: {full_path}      {file_name}")
+        logging.info(f"Processing: {full_path} \n {file_name}")
         # Docling process the file into MARKDOWN
         # returns original file path, markdown data, and filename(to be used to name the file in collection)
         #full_path, markdown, file_name = ds.get_docling_data(file, OUTPUT_DIR)
         #file_id = upload_to_milvus(full_path, file_name, json_markdown)
         #print(f"Milvus returns {file_id}")
-        file_id = upload_files(OPENWEBUI_API_KEY, f'{new_path}/{file_name}', OPEN_ENDPOINT, file_name)
-        print(file_id)
+        exists = check_file_exists(OPENWEBUI_API_KEY, OPEN_ENDPOINT, file_name)
+        if exists['detail'] == 'No files found matching the patern':
+            file_id = upload_files(OPENWEBUI_API_KEY, f'{new_path}/{file_name}', OPEN_ENDPOINT, file_name)
+            print(file_id)
+        else:
+            print(f"File {file_name} already exists, skipping upload")
+            continue
         #file_id = upload_file(OPENWEBUI_API_KEY, good_path, OPEN_ENDPOINT, file_name) #upload_to_milvus(full_path, markdown)
         a_id = file_id['id']
         response['status'] = 'incomplete'
-        while response['status'] !='completed':
+        while response['status'] !='completed' or response['status'] !='failed':
             response = check_file(a_id,OPENWEBUI_API_KEY)
             time.sleep(5.0)
         time.sleep(1.0)
-        print("****************************File Uploaded with ID*******************************************")
-        print(f"Returned: {file_id['id'],file_id['filename'],file_id['meta']['size']}")
-        #final = upload_knowledge(file_id['id'],file_id['filename'],file_id['meta']['size'],KNOWLEDGE_ENDPOINT,OPENWEBUI_API_KEY)
-        print("*****************************Send to Knowledge******************************************")
-        final = upload_knowledge(a_id,file_name,file_id['meta']['size'],KNOWLEDGE_ENDPOINT,OPENWEBUI_API_KEY)
-        print("*****************************Returned from Knowledge******************************************")
-        print(final)
-        if final == 'fail':
-            print(f"Error saving file {file_id['filename']} to knowledge base")
-        else:
-            pass
+        if response['status'] =='completed':
+            print("****************************File Uploaded with ID*******************************************")
+            print(f"Returned: {file_id['id'],file_id['filename'],file_id['meta']['size']}")
+            logging.info(f"Returned: {file_id['id'],file_id['filename'],file_id['meta']['size']}")
+            #final = upload_knowledge(file_id['id'],file_id['filename'],file_id['meta']['size'],KNOWLEDGE_ENDPOINT,OPENWEBUI_API_KEY)
+            print("*****************************Send to Knowledge******************************************")
+            final = upload_knowledge(a_id,file_name,file_id['meta']['size'],KNOWLEDGE_ENDPOINT,OPENWEBUI_API_KEY)
+            print("*****************************Returned from Knowledge******************************************")
+            print(final)
+            if final == 'fail':
+                print(f"Error saving file {file_id['filename']} to knowledge base")
+            else:
+                pass
+            
